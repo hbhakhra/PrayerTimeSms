1beeea42d0e6664717e9d2b3a11a9229c9d5d054
Humza Bhakhrani <HumzaBhakhrani@gmail.com>
2016-11-28 09:14:38 -0800

    Initialize

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyBk8XaoE1ZUy_MV229Zis3GrmyA03MhZN4",
+  authDomain: "prayer-times.firebaseapp.com",
+  databaseURL: "https://prayer-times.firebaseio.com",
+  storageBucket: "firebase-prayer-times.appspot.com",
+  messagingSenderId: "1065208052132"
+};
+
+module.exports = config;
